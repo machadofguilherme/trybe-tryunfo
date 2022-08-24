d8b0ec25bd012f8f7bc7b1451d7935612c4fe1f1
Guilherme Machado <machadofguilherme@gmail.com>
2022-08-24 18:38:27 -0300

    Requisito 8 - Encurtado.

--- src/App.js
+++ src/App.js
@@ -121,15 +121,8 @@ class App extends React.Component {
           {
             changeBool === true && onSaveButtonClick.map((cards) => (
               <Card
-                key={ cards.cardName }
-                cardName={ cards.cardName }
-                cardDescription={ cards.cardDescription }
-                cardAttr1={ cards.cardAttr1 }
-                cardAttr2={ cards.cardAttr2 }
-                cardAttr3={ cards.cardAttr3 }
-                cardImage={ cards.cardImage }
-                cardRare={ cards.cardRare }
-                cardTrunfo={ cards.cardTrunfo }
+                key={ cards }
+                { ...cards }
               />
             ))
           }
